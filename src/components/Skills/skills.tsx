e3ba1d6f0689372bb76f331d25dcc0bbd3f6ff86
Pablo <pablo.albin88@gmail.com>
2025-05-11 21:43:00 -0300

    Update skills

--- src/components/Skills/skills.tsx
+++ src/components/Skills/skills.tsx
@@ -12,25 +12,26 @@ const Skills: React.FC = () => {
           <div className={style.skill}>Angular</div>
           <div className={style.skill}>React</div>
           <div className={style.skill}>React Native</div>
+          <div className={style.skill}>TypeScript</div>
+          <div className={style.skill}>JavaScript</div>
           <div className={style.skill}>HTML5</div>
           <div className={style.skill}>CSS3</div>
-          <div className={style.skill}>JavaScript</div>
-          <div className={style.skill}>TypeScript</div>
           <div className={style.skill}>Sass</div>
           <div className={style.skill}>Bootstrap</div>
           <div className={style.skill}>Figma</div>
         </div>
         <h4 className={style.subtitle}>Backend</h4>
         <div className={style.skills_container}>
-          <div className={style.skill}>Node.js</div>
-          <div className={style.skill}>Express</div>
+          <div className={style.skill}>SpringBoot</div>
           <div className={style.skill}>NestJS</div>
           <div className={style.skill}>Laravel</div>
-          <div className={style.skill}>Spring Boot</div>
-          <div className={style.skill}>AWS</div>
+          <div className={style.skill}>GraphQL</div>
+          <div className={style.skill}>Node.js</div>
+          <div className={style.skill}>Express</div>
         </div>
         <h4 className={style.subtitle}>Database</h4>
         <div className={style.skills_container}>
+          <div className={style.skill}>Prisma</div>
           <div className={style.skill}>MySQL</div>
           <div className={style.skill}>PostgreSQL</div>
           <div className={style.skill}>Firebase</div>
@@ -38,10 +39,12 @@ const Skills: React.FC = () => {
         </div>
         <h4 className={style.subtitle}>Tools</h4>
         <div className={style.skills_container}>
+          <div className={style.skill}>AWS</div>
+          <div className={style.skill}>Docker</div>
+          <div className={style.skill}>Jenkins</div>
+          <div className={style.skill}>Scrum</div>
           <div className={style.skill}>VS Code</div>
           <div className={style.skill}>Git</div>
-          <div className={style.skill}>Docker</div>
-          <div className={style.skill}>Scrum</div>
           <div className={style.skill}>GitHub</div>
         </div>
       </div>
